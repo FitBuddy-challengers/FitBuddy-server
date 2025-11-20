de9f383e3586a250623990537704061624a788d9
Chea-yunzi <katete051@naver.com>
2025-11-21 02:52:58 +0900

    🐛 :feature/9 : 쿼리문 수정 안함. 다시 변경함

--- src/controller/exercise/checkExistingPlan.controller.js
+++ src/controller/exercise/checkExistingPlan.controller.js
@@ -11,14 +11,13 @@ export default function checkExistingPlanController({ pool }) {
         try {
           const result = await pool.query(
             `
-            SELECT id 
+            SELECT id
             FROM exercise_plan
             WHERE user_id = $1
-            AND start_date = $2
-            AND end_date = $3
+            AND $2 BETWEEN start_date AND end_date
             LIMIT 1
           `,
-            [user_id, start_date, end_date]
+            [user_id, date]
           );
   
           if (result.rows.length > 0) {
@@ -27,7 +26,7 @@ export default function checkExistingPlanController({ pool }) {
             return res.json({ exists: false });
           }
         } catch (err) {
-          console.error(" checkExistingPlan error:", err);
+          console.error("checkExistingPlan error:", err);
           return res.status(500).json({ error: "서버 오류" });
         }
       }
